f95c684436914c425eaced6bd240f38ff7451436
Amine Raouf <raouf.amine@iisgalvanimi.edu.it>
2025-05-19 12:49:50 +0000

    commit fino al punto 2

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+  data = {};
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-maliziag-dockergitpod-j364n3hn7ec.ws-eu107.gitpod.io/simple_json")
+    .subscribe(
+      (data) => 
+      {
+        this.data = data;
+        console.log(data);
+      }
+    )
+  }
+}
